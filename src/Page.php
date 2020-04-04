007de86f4a428b0a8da20efd00398aadbdd7e55a
Ondřej Maxa <ondrej.maxa.ih1@gmail.com>
2020-04-04 18:50:43 +0200

    Download conversations.

--- src/Page.php
+++ src/Page.php
@@ -4,7 +4,25 @@
 namespace Majksa\Messenger;
 
 
+use Facebook\Exceptions\FacebookSDKException;
+
 class Page extends Node
 {
+    /**
+     * @throws FacebookSDKException
+     */
+    public function getConversations(): array
+    {
+        $conversationsArray = $this->downloadConversations();
 
+    }
+
+    /**
+     * @throws FacebookSDKException
+     */
+    private function downloadConversations()
+    {
+        $response = $this->connection->execute(Connection::ME, ['conversations'=>['messages'=>['message']]], $this->token);
+        return $response->getGraphNode()->getField('conversations')->asArray();
+    }
 }
