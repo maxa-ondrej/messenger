8f9dcd6ecaa22289f504305e877ba0c391362176
Ondřej Maxa <ondrej.maxa.ih1@gmail.com>
2020-04-04 14:57:55 +0200

    Connection creation.

--- tests/unit/ConnectionTest.php
+++ tests/unit/ConnectionTest.php
@@ -1,22 +1,26 @@
 <?php
-class ConnectionTest extends \Codeception\Test\Unit
+
+use Codeception\Test\Unit;
+use Majksa\Messenger\Connection;
+
+class ConnectionTest extends Unit
 {
+    public const APP_ID = '2465923617004196';
+    public const APP_SECRET = 'a863400c265d24b719566370e422dde6';
+
     /**
      * @var \UnitTester
      */
     protected $tester;
-    
+
+    private $connection;
+
     protected function _before()
     {
+        $this->connection = new Connection(self::APP_ID, self::APP_SECRET);
     }
 
     protected function _after()
     {
-    }
-
-    // tests
-    public function testSomeFeature()
-    {
-
     }
 }
